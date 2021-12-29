8b1a56091714eca1bdabc350dbdcb429270d922f
qsummerford <qsummerford22@basecampcodingacademy.org>
2021-12-29 15:59:41 -0600

    images and button

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-
+var under_sign_in = document.querySelector(".sign_in");
+const sign_in_button = document.querySelector(".person");
+sign_in_button.addEventListener("click", () => {
+  under_sign_in.style.display =
+    under_sign_in.style.display === "block" ? "block" : "none";
+});
