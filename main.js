87cc4f90e5d91882f3284162eed74a52f9be46d6
qsummerford <qsummerford22@basecampcodingacademy.org>
2022-01-04 16:06:11 -0600

    did some big changes

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-var under_sign_in = document.querySelector(".sign_in");
+const under_sign_in = document.querySelector("#sign_in");
 const sign_in_button = document.querySelector(".person");
+under_sign_in.style.display = "none";
 sign_in_button.addEventListener("click", () => {
-  under_sign_in.style.display =
-    under_sign_in.style.display === "block" ? "block" : "none";
+  if (under_sign_in.style.display == "none") {
+    under_sign_in.style.display = "block";
+  } else {
+    under_sign_in.style.display = "none";
+  }
+  under_sign_in.style.position = "relative";
+  under_sign_in.style.float = "right";
+  under_sign_in.style.marginRight = "10px";
+  under_sign_in.style.backgroundColor = "black";
+  under_sign_in.style.color = "white";
+  under_sign_in.style.padding = "5px";
+});
+
+var hidden_cars = document.querySelector("#next");
+hidden_cars.style.display = "none";
+const next_arrow = document.querySelector("#next_arrow");
+const prev_cars = document.querySelector("#prev");
+
+next_arrow.addEventListener("click", () => {
+  if (hidden_cars.style.display == "none") {
+    hidden_cars.style.display = "block";
+  } else {
+    hidden_cars.style.display = "none";
+  }
+  prev_cars.style.display = "none";
+  next_arrow.style.display = "none";
 });
