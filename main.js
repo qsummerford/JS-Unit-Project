482f1235d70efbf16e7fa200aaa34e3afbfe912b
qsummerford <qsummerford22@basecampcodingacademy.org>
2022-01-05 16:22:29 -0600

    more big changes

--- main.js
+++ main.js
@@ -19,13 +19,64 @@ var hidden_cars = document.querySelector("#next");
 hidden_cars.style.display = "none";
 const next_arrow = document.querySelector("#next_arrow");
 const prev_cars = document.querySelector("#prev");
+const prev_arrow = document.querySelector("#prev_arrow");
+prev_arrow.style.display = "none";
+
+const next_cars = document.querySelector("#next2");
+next_cars.style.display = "none";
+
+const next_arrow2 = document.querySelector("#next_arrow2");
+next_arrow2.style.display = "none";
+
+const prev_arrow2 = document.querySelector("#prev_arrow2");
+prev_arrow2.style.display = "none";
 
 next_arrow.addEventListener("click", () => {
   if (hidden_cars.style.display == "none") {
-    hidden_cars.style.display = "block";
+    hidden_cars.style.display = "flex";
+    prev_arrow.style.display = "block";
+    next_arrow2.style.display = "block";
   } else {
     hidden_cars.style.display = "none";
   }
   prev_cars.style.display = "none";
   next_arrow.style.display = "none";
+});
+
+prev_arrow.addEventListener("click", () => {
+  if (prev_cars.style.display == "none") {
+    prev_cars.style.display = "flex";
+    next_arrow.style.display = "block";
+  } else {
+    prev_cars.style.display = "none";
+  }
+
+  hidden_cars.style.display = "none";
+  prev_arrow.style.display = "none";
+  next_arrow2.style.display = "none";
+});
+
+next_arrow2.addEventListener("click", () => {
+  if (next_cars.style.display == "none") {
+    next_cars.style.display = "flex";
+    prev_arrow2.style.display = "block";
+  } else {
+    next_cars.style.display = "none";
+  }
+  hidden_cars.style.display = "none";
+  next_arrow2.style.display = "none";
+  prev_arrow.style.display = "none";
+  next_arrow.style.display = "none";
+});
+
+prev_arrow2.addEventListener("click", () => {
+  if (hidden_cars.style.display == "none") {
+    hidden_cars.style.display = "flex";
+    next_arrow2.style.display = "block";
+    prev_arrow.style.display = "block";
+  } else {
+    hidden_cars.style.display = "none";
+  }
+  prev_arrow2.style.display = "none";
+  next_cars.style.display = "none";
 });
